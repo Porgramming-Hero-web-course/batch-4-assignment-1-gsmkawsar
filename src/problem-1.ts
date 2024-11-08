a087effa366b21b19e1f10e0d1320a295497a25e
gsmkawsar <support@bestlashfile.com>
2024-11-08 15:10:14 +0600

    problem-1 solve

---
+++ src/problem-1.ts
@@ -0,0 +1,13 @@
+{
+    // problem 1
+
+    function sumArray(num: number[]): number {
+        return num.reduce((acc, value) => acc + value, 0);
+    }
+
+    console.log(sumArray([1, 2, 3, 4, 5]));
+
+
+    // 
+
+}
