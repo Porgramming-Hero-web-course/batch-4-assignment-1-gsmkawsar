ccd0af2c42d6f4f4756951ad1b0f0ce1dad36afc
gsmkawsar <support@bestlashfile.com>
2024-11-08 15:22:00 +0600

    problem-2 solve

---
+++ src/problem-2.ts
@@ -0,0 +1,18 @@
+{
+    // problem 2 
+
+    function removeDuplicates(arr: number[]): number[] {
+        const result: number[] = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (!result.includes(arr[i])) {
+                result.push(arr[i])
+            }
+        }
+        return result;
+    }
+
+    console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+  
+    // 
+}
