54c7fb7120a4a24ae0a5af898dd2952dd1087ac0
gsmkawsar <support@bestlashfile.com>
2024-11-08 22:08:15 +0600

    problem-3 solve

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,4 +1,17 @@
 {
-    // 
-    // 
-}
+
+    // problem 3
+    function countWordOccurrences(words: string, searchItem: string): void {
+      const wordArray: string[] = words.split(" ");
+  
+      let count = 0;
+      for (const word of wordArray) {
+        if (word === searchItem) {
+          count++;
+        }
+      }
+      console.log(count);
+    }
+  
+    countWordOccurrences("I love typescript", "typescript");
+  }
